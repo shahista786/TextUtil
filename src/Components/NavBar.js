b3f681195439e0160e3b1c1d33fda6d4b14a0c0b
Shahista Shaikh <shahistashaikh000786@gmail.com>
2024-03-25 13:40:59 +0530

    code pushed

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -29,7 +29,7 @@ export default function NavBar(props) {
                 </a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <a className="nav-link active" href="/">
                   {props.about}
                 </a>
               </li>
